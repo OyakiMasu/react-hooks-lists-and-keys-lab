75c43a769b7875f1a0293e08724c2d3bdb571c96
OyakMasu21 <akzaki17@gmail.com>
2023-01-13 10:02:57 +0300

    Errors

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
-function ProjectList({ projects }) {
-  console.log(projects);
+function ProjectList({ projects  }) {
+  console.log(projects[0].id);
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list" >
+        {/* render ProjectItem components here */}
+       {projects.map(project => (
+        // <h1 key={project.id}>{project.name}</h1>
+        <ProjectItem key={project.id} project={project}/>
+
+       ))}
+
+      </div>
     </div>
   );
 }
